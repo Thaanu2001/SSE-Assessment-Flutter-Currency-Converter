6f390440d1192416f0bd1f79500380722c4291ef
Thaanu Perera <thaanup@gmail.com>
2025-01-04 23:02:18 +0530

    Update API key placeholder in ApiConstants for security

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,4 +1,4 @@
 class ApiConstants {
   static const String BASE_URL = 'https://v6.exchangerate-api.com/v6/';
-  static const String API_KEY = '37c2906b1c97f3d690d43608';
+  static const String API_KEY = '________YOUR_API_KEY________';
 }
